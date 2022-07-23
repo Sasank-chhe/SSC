1ba07ed27e257dda5db5415a28a9d45162f2c4dc
Sasank-Ss4 <99401760+Sasank-chhe@users.noreply.github.com>
2022-07-24 02:52:55 +0545

    Create SSC04.py

---
+++ SSC04.py
@@ -0,0 +1,25 @@
+import os, platform, time
+
+print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+
+def Update():
+
+    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
+
+def Run():
+
+        bit = platform.architecture()[0]
+
+        if bit == '64bit':
+
+            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+
+            from SSC import Menu
+
+            login()
+
+        else:
+
+            exit('\033[1;31m[×] Device Not Support 32bit')
+
+Run()
